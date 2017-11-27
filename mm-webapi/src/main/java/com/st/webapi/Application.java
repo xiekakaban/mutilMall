0139ce4da0086d0a47ce340a59c388a9bd3b547e
Sanmu <1215148017@qq.com>
2017-11-27 21:32:50 +0800

    add logging properties

--- mm-webapi/src/main/java/com/st/webapi/Application.java
+++ mm-webapi/src/main/java/com/st/webapi/Application.java
@@ -3,34 +3,28 @@ package com.st.webapi;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
- * @author liuzh
- * @since 2015-12-12 18:22
+ * @author Sanmu.
+ * @date 11/26/2017
+ * @email 1215148017@qq.com
  */
 @Controller
-@EnableWebMvc
 @SpringBootApplication
 @ComponentScan("com.st")
 @MapperScan(basePackages = "com.st.dao.customized")
-public class Application extends WebMvcConfigurerAdapter implements CommandLineRunner {
+@PropertySource(ignoreResourceNotFound = true, value = "classpath:application-dao.properties")
+public class Application {
     private Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        logger.info("服务启动完成!");
     }
 
     @RequestMapping("/")
